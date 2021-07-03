12f8d19a801297145ca38a1d300ab8846cf5d224
Ashwini <Ashwini@MY-PC>
2021-07-03 18:11:32 +0530

    Adding TC01_Clients and TC02_Clients

--- InvoiceNinja/src/test/java/clients/TC01_ClientsTest.java
+++ InvoiceNinja/src/test/java/clients/TC01_ClientsTest.java
@@ -19,7 +19,10 @@ public class TC01_ClientsTest extends BaseTest{
 	@Test
 	public void clients() {
 		
-		//Step1:Click on "clients" link.
+		 ExcelUtil readExcelData=new ExcelUtil();
+		
+		//Step1:Click on "clients" link
+		 
 		HomePage homepage=new HomePage(driver);
         homepage.getClientLink().click();
         String expectedClientsPageTitle = "Clients | Invoice Ninja";
@@ -27,6 +30,7 @@ public class TC01_ClientsTest extends BaseTest{
         Reporter.log("Clients page is displayed",true);
         
         //Step2:Click on "new clients" button
+        
         ClientsPage clientspage=new ClientsPage(driver);
         clientspage.getnewClients().click();
         String expectedNewClientPageTitle = "New Client | Invoice Ninja";
@@ -34,11 +38,20 @@ public class TC01_ClientsTest extends BaseTest{
         Reporter.log("New Clients page is displayed",true);
         
         //Step3:Enter valid data in details box and click on "save" button
+        
         NewClientsPage newclient =new NewClientsPage(driver);
-        newclient.getName().sendKeys("TestYantra");
-        newclient.getIdNumber().sendKeys("12345");
-        newclient.getVatNumber().sendKeys("456");
-        newclient.getphone1().sendKeys("1234567890");
+        String name = readExcelData.readStringDataFromExcel("Clients", 1, 0);
+        newclient.getName().sendKeys(name);
+        
+        double idNumber = readExcelData.readNumberDataFromExcel("Clients", 1,1);
+        newclient.getIdNumber().sendKeys(""+idNumber+"");
+        
+        double vatNumber = readExcelData.readNumberDataFromExcel("Clients", 1, 2);
+        newclient.getVatNumber().sendKeys(""+vatNumber+"");
+        
+        double phone = readExcelData.readNumberDataFromExcel("Clients", 1, 3);
+        newclient.getphone1().sendKeys(""+phone+"");
+        
         newclient.getSaveButton().click();	
         String expectedViewClientPageTitle = "View Client | Invoice Ninja";
         Assert.assertEquals(driver.getTitle(),expectedViewClientPageTitle,"Information is not saved");
@@ -46,3 +59,4 @@ public class TC01_ClientsTest extends BaseTest{
         
 	}
 }
+
--- InvoiceNinja/src/test/java/clients/TC02_ClientsTest.java
+++ InvoiceNinja/src/test/java/clients/TC02_ClientsTest.java
@@ -21,14 +21,16 @@ public class TC02_ClientsTest extends BaseTest {
      @Test
     public void editClient() {
     	 
-    	//Step1:Click on "clients" link.
+    	//========Step1:Click on "clients" link=========//
+    	 
 	   HomePage homepage=new HomePage(driver);
        homepage.getClientLink().click();
        String expectedClientsPageTitle = "Clients | Invoice Ninja";
        Assert.assertEquals(driver.getTitle(),expectedClientsPageTitle,"Clients page is not displayed");
        Reporter.log("Clients page is displayed",true);
     
-       //Step2:Click on new select drop down and click on "edit clients"
+       //========Step2:Click on new select drop down and click on "edit clients"=======//
+       
        ClientsPage clientspage=new ClientsPage(driver);
        Actions actions=new Actions(driver);
        actions.moveToElement(driver.findElement(By.xpath("//center[@style='min-width:100px']"))).perform();
@@ -38,11 +40,18 @@ public class TC02_ClientsTest extends BaseTest {
        Assert.assertEquals(driver.getTitle(),expectedEditClientsPageTitle,"Edit Clients page is not displayed");
        Reporter.log("Edit Clients page is displayed",true);
        
-       //Step3:Enter valid data in contacts box and click on "save" button
+       //========Step3:Enter valid data in contacts box and click on "save" button========//
+       
        NewClientsPage newclients=new NewClientsPage(driver);
-       newclients.getfirstName().sendKeys("sumanth");
-       newclients.getLastName().sendKeys("m");
-       newclients.getphone1().sendKeys("1234567890");
+       String firstName = readExcelData.readStringDataFromExcel("Clients", 1, 4);
+       newclients.getfirstName().sendKeys(firstName);
+       
+       String LastName = readExcelData.readStringDataFromExcel("Clients", 1, 5);
+       newclients.getLastName().sendKeys(LastName);
+       
+       double phone = readExcelData.readNumberDataFromExcel("Clients", 1,6);
+       newclients.getphone1().sendKeys(""+phone+"");
+       
        newclients.getSaveButton().click();
        String expectedViewClientPageTitle = "View Client | Invoice Ninja";
        Assert.assertEquals(driver.getTitle(),expectedViewClientPageTitle,"Information is not saved");
